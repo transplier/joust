90173c958a7d8ae59e4158cc98a04eaa88652431
Giacomo Ferrari <giacomo@scribus.(none)>
2008-12-28 19:05:02 -0600

    Added known failing test for detecting HEX

--- src-test/com/dgis/JOuST/serial/ElmSerialTest.java
+++ src-test/com/dgis/JOuST/serial/ElmSerialTest.java
@@ -66,6 +66,16 @@ public class ElmSerialTest {
 		ret = test.process_response(null, buf);
 		assertTrue((ret == ELMResponse.HEX_DATA));
 
+		buf = new byte[] { 52, 49, 32, 48, 48, 32, 57, 56, 32, 49, 56, 32, 56,
+				48, 32, 48, 49, 32, 13, 52, 49, 32, 48, 48, 32, 66, 69, 32, 51,
+				70, 32, 69, 56, 32, 49, 51, 32, 13, 13, 62, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+		ret = test.process_response("0100".getBytes(), buf);
+		assertTrue((ret == ELMResponse.HEX_DATA));
+
 		strcpy(buf, "<DATA ERROR>");
 		ret = test.process_response(null, buf);
 		assertTrue((ret == ELMResponse.DATA_ERROR2));
