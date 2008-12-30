25a07b975ddc9331c33414b0cf270ea69c4b2d9b
Giacomo Ferrari <progman32@gmail.com>
2008-12-30 00:56:06 -0600

    Added simple throttle reading to TestConnectAndID.

--- src-test/com/dgis/JOuST/serial/TestConnectAndID.java
+++ src-test/com/dgis/JOuST/serial/TestConnectAndID.java
@@ -21,6 +21,11 @@ public class TestConnectAndID {
 			ELMResponse device = underTest.getDevice();
 			outln("Detected interface: "+(device==null?"None":device.toString()));
 			Thread.sleep(1000);
+			//Try getting throttle position as a test.
+			byte[] result = underTest.request_pid(0x11, 1);
+			float rawPos = (float)Integer.valueOf(ElmSerial.bytesToString(result).substring(4), 16);
+			rawPos*=100f/255f;
+			outln("Throttle position: "+rawPos);
 			outln("Closing port...");
 			underTest.close_comport();
 		} catch (Exception e){
